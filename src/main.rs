f98e3f5f010a8bf4e064dd4862b68ffa96625911
anjali gupta <sajalanjali3@gmail.com>
2025-02-19 13:54:12 +0530

    Temperature Converter

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
+/* fn main() {
+    #[derive(Debug)]
+    struct Employee {
+        name:String,
+        age:u32,
+        salary:f64
+    }
+    impl Employee {
+        fn get_details(&self) {
+            println!("the employe name is {} and age is {} and earn {}",self.name,self.age,self.salary);
+        }
+        fn increase_salary(&mut self,amount:f64){
+            self.salary += amount;
+            println!("salary is increased with {}",self.salary);
+        }
+    }
+    let mut anjali:Employee = Employee{
+        name: String::from("Anjali"),
+        age:22,
+        salary:5000.0
+    };
+    println!("{:?}" , anjali);
+    anjali.get_details();
+    anjali.increase_salary(40000.0);
+} */
+
+/* Temperature Converter */
 use std::io;
 fn main() {
-     let mut operation:String = String::new();
-     let mut num1:String = String::new();
-     let mut num2:String = String::new();
+    println!("welcome to price converter💫");
+    let mut celcius: String = String::new();
+    println!("what Temperature you want to convert inter below ");
+    io::stdin().read_line(&mut celcius).expect("invalid input");
+    let celcius:f64 = celcius.trim().parse().expect("not converted");
+    let fahrenheit = celcius *1.8 + 32.0;
+    println!("fahrenheit {}" , fahrenheit);
 
-     println!("enter the operation");
-     io::stdin().read_line(&mut operation).expect("failed to take input");
-     println!("enter the first number");
-     io::stdin().read_line(&mut num1).expect("failed to take input");
-     println!("enter the second  number");
-     io::stdin().read_line(&mut num2).expect("failed to take input");
-     
-     let num1:i32 = num1.trim().parse().expect("something went wrong");
-     let num2:i32 = num2.trim().parse().expect("something went wrong");
-     let operation = operation.trim(); 
-
-
-     let result = match operation {
-        "+" => num1 +num2,
-        "*" => num1*num2,
-        "-" => num1 - num2,
-        "/" => num1 / num2,
-        _=> {
-            println!("invalid operation");
-            return 
-        }
-     };
-     println!("{}", result);
-
-
-} 
+}
